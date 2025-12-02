1ed2f3be432440caa6a60df70fb98beb8966e3f5
ShahdAboLatif <shahdabolatif@gmail.com>
2025-12-02 12:16:55 -0500

    seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'audit',
                 'email' => 'audit@example.com',
+                'role' => 'Admin',
                 'password' => 'auditpassword',
                 'email_verified_at' => now(),
             ]
